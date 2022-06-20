e8cee917c73a20839488890b5d21d82c01485740
Andrew Lee <alee68@fordham.edu>
2022-06-19 21:56:52 -0400

    init commit with index.js and class files created

---
+++ engineer.js
@@ -0,0 +1,9 @@
+class Engineer {
+	constructor(id, email, github) {
+		this.id = id;
+		this.email = email;
+		this.github = github;
+	}
+}
+
+module.exports = Engineer;
---
+++ index.js
@@ -0,0 +1,8 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+
+const questions = [{}];
+
+inquirer.prompt([questions]).then((answers) => {
+	fs;
+});
